bc0d57d24eeed2dfc7e87be0fb0bf888a5f1ecff
James <james.re@yahoo.com>
2020-02-26 14:36:01 -0800

    added lib/auth/files

--- server.js
+++ server.js
@@ -8,6 +8,31 @@ const morgan = require('morgan');
 const client = require('./lib/client');
 // Initiate database connection
 client.connect();
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash, display_name as "displayName" 
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "displayName";
+        `,
+        [user.email, hash, user.displayName]
+        ).then(result => result.rows[0]);
+    }
+});
+
 
 // Application Setup
 const app = express();
@@ -17,9 +42,9 @@ app.use(cors()); // enable CORS request
 app.use(express.static('public')); // server files from /public folder
 app.use(express.json()); // enable reading incoming json data
 app.use(express.urlencoded({ extended: true }));
+
+
 // API Routes
-
-
 // *** TODOS ***
 // this is /GET request that returns whole list of todos
 app.get('/api/todos', async (req, res) => {
